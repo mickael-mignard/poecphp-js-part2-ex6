46a746c4178a65d4185768c1212d289555010c63
Mickael Mignard <mignard.mickael@gmail.com>
2021-05-26 10:04:37 +0200

    Simplification du code

--- js/script.js
+++ js/script.js
@@ -1,36 +1,20 @@
-/**
- * Change image when mouse enters
- */
- (function(){
-
-    let images = document.querySelectorAll('img');     
-
-    images.forEach( elt =>  {        
-       
-        elt.addEventListener('mouseenter', function changeImage(){
-            let attributeSrc = this.getAttribute('src');
-            let rootPath = attributeSrc.substring(0, attributeSrc.length-4);            
-            this.setAttribute('src', rootPath + '_2.jpg');            
-        })
-
-    });       
-}());
+let images = document.querySelectorAll('img');
 
 /**
- * Put back original image when mouse leaves
+ * Change image when mouse enters 
  */
-(function(){
+ images.forEach(elt => {
+   elt.addEventListener('mouseover', function () {
+     elt.src = 'images/' + elt.getAttribute('id')+'_2.jpg';
+   });
+ });
 
-    let images = document.querySelectorAll('img');     
-
-    images.forEach( elt =>  {        
-       
-        elt.addEventListener('mouseleave', function putBackImage(){
-            let attributeSrc = this.getAttribute('src');
-            let rootPath = attributeSrc.substring(0, attributeSrc.length-6);            
-            this.setAttribute('src', rootPath + '.jpg');                   
-        })
-
-    });       
-}());
+ /**
+ * Change image when mouse leaves 
+ */
+  images.forEach(elt => {
+    elt.addEventListener('mouseleave', function () {
+      elt.src = 'images/' + elt.getAttribute('id') + '.jpg';
+    });
+  });
 
